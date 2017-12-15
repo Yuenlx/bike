ff720c6f31e7cd4a68d2c3d974c189f1433d3a23
Swift <yuanlx@fruitday.com>
2017-12-15 13:03:30 +0000

    Prevent XSS injection

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -22,6 +22,9 @@ class TopicObserver
     // 每个数据模型都有生命周期，周期里会有很多事件(creating、...saved...)
     public function saving(Topic $topic)
     {
+        // 当不使用simditor时，使用 HTMLPurifier for Laravel 5 的XSS配置过滤规则
+        $topic->body = clean($topic->body, 'user_topic_body');
+
         // 在话题模型保存是，生成话题摘要。自定义辅助函数 make_excerpt 写在 helpers.php 文件中
         $topic->excerpt = make_excerpt($topic->body);
     }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'Bike') | 科技项目布道社区 ！</title>
+    <title>@yield('title', 'Bike') | 科技项目布道众创社区 ！</title>
     <meta name="description" content="@yield('description', '新科技项目布道社区')"/>
 
     <!-- Styles -->
